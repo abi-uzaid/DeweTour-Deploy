d5c02df42e35cf91a92d069f0e1b222aba60386c
abiuzaid <abiuzaid1427@gmail.com>
2023-06-12 08:52:59 +0700

    perbaikan

--- server/dto/transaction/transaction_request.go
+++ server/dto/transaction/transaction_request.go
@@ -4,10 +4,10 @@ import "dewetour/models"
 
 type createTransactionRequest struct {
 	ID         int         `json:"id"`
-	CounterQty int         `json:"counter_qty" validate:"required" form:"counter_qty"`
-	Total      int         `json:"total" validate:"required" form:"total"`
-	Status     string      `json:"status" validate:"required" form:"status"`
-	TripID     int         `json:"trip_id" validate:"required" form:"trip_id"`
+	CounterQty int         `json:"counter_qty" validate:"required" `
+	Total      int         `json:"total" validate:"required" `
+	Status     string      `json:"status" validate:"required" `
+	TripID     int         `json:"trip_id" validate:"required" `
 	Trip       models.Trip `json:"trip"`
 	UserID     int         `json:"user_id"`
 	User       models.User `json:"user"`
@@ -15,10 +15,10 @@ type createTransactionRequest struct {
 
 type UpdateTransactionRequest struct {
 	ID         int         `json:"id"`
-	CounterQty int         `json:"counter_qty" form:"counter_qty"`
-	Total      int         `json:"total" form:"total"`
-	Status     string      `json:"status" form:"status"`
-	TripID     int         `json:"trip_id" form:"trip_id"`
+	CounterQty int         `json:"counter_qty" `
+	Total      int         `json:"total" `
+	Status     string      `json:"status" `
+	TripID     int         `json:"trip_id" `
 	Trip       models.Trip `json:"trip"`
 	UserID     int         `json:"user_id"`
 	User       models.User `json:"user"`
