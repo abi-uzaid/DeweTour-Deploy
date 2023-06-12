2c84bc349efc18210d738fea3058c307e9333f04
abiuzaid <abiuzaid1427@gmail.com>
2023-06-12 09:27:05 +0700

    perbaikan

--- server/dto/transaction/transaction_request.go
+++ server/dto/transaction/transaction_request.go
@@ -4,10 +4,10 @@ import "dewetour/models"
 
 type CreateTransactionRequest struct {
 	ID         int         `json:"id"`
-	CounterQty int         `json:"counter_qty" validate:"required" `
-	Total      int         `json:"total" validate:"required" `
-	Status     string      `json:"status" validate:"required" `
-	TripID     int         `json:"trip_id" validate:"required" `
+	CounterQty int         `json:"counter_qty"`
+	Total      int         `json:"total"`
+	Status     string      `json:"status"`
+	TripID     int         `json:"trip_id"`
 	Trip       models.Trip `json:"trip"`
 	UserID     int         `json:"user_id"`
 	User       models.User `json:"user"`
