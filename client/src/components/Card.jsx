ca681cd0adfad1f4d83a3c03519c5c1bc1d5f522
abiuzaid <abiuzaid1427@gmail.com>
2023-06-12 11:27:59 +0700

    perbaikan

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -6,9 +6,7 @@ import { useQuery } from "react-query";
 import { API } from "../config/api";
 
 const Cards = ({ data, search }) => {
-  console.log(data, "ini data kontol doni ");
-  // console.log(search, "ini search ");
-
+ 
   const rupiah = (number) => {
     return new Intl.NumberFormat("id-ID", {
       style: "currency",
